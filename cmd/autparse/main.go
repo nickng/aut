2eaf0b2dc3a33cd5e994b782584dc79883b32dd0
Nicholas Ng <nickng@nickng.io>
2017-03-19 19:03:09 +0000

    autparse: Add option to read from file.

--- cmd/autparse/main.go
+++ cmd/autparse/main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/nickng/aut"
 )
 
+var (
+	infile string
+)
+
+func init() {
+	flag.StringVar(&infile, "in", "", "Input file to read from (Default: stdin)")
+}
+
 func main() {
-	r := bufio.NewReader(os.Stdin)
+	flag.Parse()
+	var r io.Reader
+	if infile != "" {
+		b, err := ioutil.ReadFile(infile)
+		if err != nil {
+			log.Fatal("Cannot read file:", err)
+		}
+		r = bytes.NewReader(b)
+	} else {
+		r = bufio.NewReader(os.Stdin)
+	}
 	parsed, err := aut.Parse(r)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Parse failed:", err)
 	}
 	fmt.Println(parsed.String())
 }
